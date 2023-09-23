de87ff9f6be62cf0e243fc92653b1e24ffde0508
Daniel Raoli <danielraoli074@gmail.com>
2023-09-22 22:07:47 -0300

    ajustind validation password

--- script.js
+++ script.js
@@ -6,58 +6,57 @@ const senhaConfirma = document.querySelector('#senhaConfirma');
 
 const caixaEmail = document.querySelector('#caixaEmail')
 
-const span =`<span  style="color: red;"> os emails não coincidem</span>`;
+const spanEmail = `<span  style="color: red;"> os emails não coincidem</span>`;
+
+const spanSenha = `<span  style="color: red;"> as senhas não coincidem</span>`;
 
 let igual = true;
 
 emailConf.addEventListener('keydown', e => {
-    
-  verificaEmail(email.value, emailConf.value);
-} );
 
-
-senhaConfirma.addEventListener('keydown', e => {
- verificaSenha(senhaConfirma.value, senha.value);
+    verificaEmail(email.value, emailConf.value);
 });
 
 
-const verificaEmail = (email, emailConfirma)=> {    
+senhaConfirma.addEventListener('keydown', e => {
+    verificaSenha(senhaConfirma.value, senha.value);
+});
+
+
+const verificaEmail = (email, emailConfirma) => {
     console.log(igual)
     console.log(email)
     console.log(emailConfirma)
-    if(emailConfirma !== email){
-        if(igual !== false){
-            caixaEmail.insertAdjacentHTML("afterbegin",span)
+    if (emailConfirma !== email) {
+        if (igual !== false) {
+            caixaEmail.insertAdjacentHTML("afterbegin", spanEmail)
             igual = false;
         }
     }
-    else{
-        
-        if(igual !== true){
+    else {
+
+        if (igual !== true) {
             igual = true;
             caixaEmail.firstElementChild.remove();
         }
 
 
     }
-    
+
+}
+
+
+const verificaSenha = (senhaConf, senha) => {
+    if (senhaConf !== senha) {
+        if (igual !== false) {
+            caixaSenha.insertAdjacentHTML("afterbegin", spanSenha)
+            igual = false;
+        }
     }
-
-
-    const verificaSenha = (senhaConf, senha)=> {    
-        if(senhaConf !== senha){
-            if(igual !== false){
-                caixaSenha.insertAdjacentHTML("afterbegin",span)
-                igual = false;
-            }
+    else {
+        if (igual !== true) {
+            igual = true;
+            caixaSenha.firstElementChild.remove();
         }
-        else{
-            if(igual !== true){
-                igual = true;
-                caixaSenha.firstElementChild.remove();
-                console.log(caixaSenha.firstElementChild)
-            }
-    
-    
-        }
+    }
 }
